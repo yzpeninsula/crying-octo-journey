dfdde09159237c03fd977353d74561154bd1bb54
yzpeninsula <yzpeninsula@gmail.com>
2018-09-07 18:13:51 +0800

    .

--- script/game/main.js
+++ script/game/main.js
@@ -1,4 +1,27 @@
 let __main = function() {
+	/////
+	let circleBody = new p2.Body({
+		mass: 5,
+		position: [0, 10],
+	})
+	let circleShape = new p2.Circle({
+		radius: 1,
+	})
+	circleBody.addShape(circleShape)
+
+	let groundBody = new p2.Body({
+		mass: 0,
+	})
+	let groundShpae = new p2.Plane()
+	groundBody.addShape(groundShpae)
+
+	let world = new p2.World({
+		gravity: [0, -9.82],
+	})
+	world.addBody(circleBody)
+	world.addBody(groundBody)
+	/////
+
 	let game = Game()
 
 	let paddle = Paddle()
@@ -26,6 +49,13 @@ let __main = function() {
 		actionManager.executePressActions()
 
 		ball.move()
+
+		world.step(1 / game.fps)
+		log(circleBody.position[0])
+		log(circleBody.position[1])
+		log(circleBody.angle)
+
+		
 
 		canvasManager.draw()
 	}
